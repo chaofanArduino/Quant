f9faff7e335d313622076fda0948a494e0a33e0b
chenchaofan <chaofan_arduino@163.com>
2020-08-01 00:23:39 +0800

    creat

---
+++ data_download.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+import tushare as ts
+
+
+class Downloader:
+    def __init__(self):
+        self.pro = ts.pro_api('66167463c58caad5dc528418ee1b52f3725a821588408c61b5915d92')
